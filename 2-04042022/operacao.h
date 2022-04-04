1ebe089105c0cbb63e88885f2d4b18fafe650025
Tayna Galbiate <tayna.galbiate@gmail.com>
2022-04-04 11:42:02 -0300

    Criaçao das funçoes

---
+++ 2-04042022/operacao.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using std::cout;
+using std::cin;
+using std::string;
+
+class Operacoes{
+    public:
+        void caadastro(string nome, string senha, string email,string nivel){               //Void não retorna
+        cout<< "Cadastro de usuário realizado\n";
+        }
+        string login(string nome, string senha){
+            return "\nSeja Bem-Vundo\n";
+        }
+        string trocarSenha(string nome, string senha){
+            return "\nSenha alterada\n";
+        }
+};
