a7e6d94000392f541abe96fc7f00caaa49b81740
Tayna Galbiate <tayna.galbiate@gmail.com>
2022-04-04 11:09:03 -0300

    Criaçao da classe Usuario

---
+++ 2-04042022/usuario.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using std::cout;
+using std::cin;
+using std::string;
+
+class Usuario{                          //Nome de classe sempre letra maiúscula
+
+    public:
+    string nome;
+    string senha;
+    string email;
+    string nivel;
+};
